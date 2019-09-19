9dfb8d998c6f6e62090ab6294b7fe8bbc04b1678
shephalis1234 <shephalis123@gmail.com>
2019-09-19 15:13:17 +0530

    jjk

--- src/main/java/jenkingit123/jenkingit123/App.java
+++ src/main/java/jenkingit123/jenkingit123/App.java
@@ -8,9 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World112!" );
-
-        System.out.println( "Hello World!122112" );
+    
+System.out.println( "Hello World!122112" );
 
     }
 }
